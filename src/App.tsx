ef97303e373a3b7a7088df5fc7809bcdbe4e8efd
donifz <doniattokurovich@gmail.com>
2021-06-26 13:58:39 +0600

    Don h1

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,19 @@ function App() {
   }
   useEffect(()=>{
     stage.render(canva.current)
+
   },[])
   useEffect(()=>{
     stage.textRender(text)
     stage.render(canva.current)
+
   },[text])
   return (
     <div className="App">
       <h1>Don</h1>
       <input type="text"  onChange={handleChange}/>
         <canvas ref={canva} id="canvas"/>
+        <h2>Don Branch</h2>
     </div>
   );
 }
